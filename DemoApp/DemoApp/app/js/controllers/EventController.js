d371d5c2e2039d85c93a96489f05235d17feb7d6
Piotr Justyna <piotr.justyna@gmail.com>
2017-11-27 21:57:12 +0000

    Filters.

--- DemoApp/DemoApp/app/js/controllers/EventController.js
+++ DemoApp/DemoApp/app/js/controllers/EventController.js
@@ -2,6 +2,12 @@
 
 eventsApp.controller('EventController',
     function EventController($scope) {
+        $scope.snippet = '<span style="color:red">hi there</span>';
+        $scope.bool = true;
+        $scope.mystyle = { color: 'red' };
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+        $scope.sortOrder = '-upvoteCount';
         $scope.event = {
             name: 'Event Name',
             date: '2017/11/23',
@@ -18,23 +24,34 @@ eventsApp.controller('EventController',
                     creatorName: 'Creator Name 1',
                     duration: 'Duration 1',
                     level: 'Level 1',
-                    abstract: 'Abstract 1'
+                    abstract: 'Abstract 1',
+                    upvoteCount: 0
                 },
                 {
                     name: 'Session 2',
                     creatorName: 'Creator Name 2',
                     duration: 'Duration 2',
                     level: 'Level 2',
-                    abstract: 'Abstract 2'
+                    abstract: 'Abstract 2',
+                    upvoteCount: 0
                 },
                 {
                     name: 'Session 3',
                     creatorName: 'Creator Name 3',
                     duration: 'Duration 3',
                     level: 'Level 3',
-                    abstract: 'Abstract 3'
+                    abstract: 'Abstract 3',
+                    upvoteCount: 0
                 },
             ]
         }
+
+        $scope.upVoteSession = function (session) {
+            session.upvoteCount++;
+        };
+
+        $scope.downVoteSession = function (session) {
+            session.upvoteCount--;
+        };
     }
 );
